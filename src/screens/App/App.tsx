b140d8808aaad04fe29b8b49dfbc0ba5d89e2750
chanlylytouch <chanlylytouch8@gmail.com>
2022-03-24 21:03:22 +0700

    sign up page

--- src/screens/App/App.tsx
+++ src/screens/App/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ContactUs from '../ContactUs';
 import IndexPage from '../indexPage';
 import AboutUs from '../AboutUs';
+import SignIn from '../SignIn';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<IndexPage/>}></Route>
           <Route path="/contact-us" element={<ContactUs />}></Route>
           <Route path="/about-us" element={<AboutUs />}></Route>
+          <Route path="/sign-in" element={<SignIn />}></Route>
         </Route>
       </Routes>
       </BrowserRouter>
